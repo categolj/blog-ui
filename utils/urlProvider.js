ca07a7502db140513d3d964575ddfc18ee3a1521
Toshiaki Maki <tmaki@vmware.com>
2022-07-25 01:15:49 +0900

    Fix urlMap

--- utils/urlProvider.js
+++ utils/urlProvider.js
@@ -1,5 +1,8 @@
-const urlMap = {
+const urlMap = (typeof window === 'object') ? {
     BLOG_API: (process.env.REACT_APP_BLOG_API && process.env.REACT_APP_BLOG_API.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname.replace(".", "-api.")}` : process.env.REACT_APP_BLOG_API || 'https://api.ik.am',
     BLOG_UI: (process.env.REACT_APP_BLOG_UI && process.env.REACT_APP_BLOG_UI.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname}` : process.env.REACT_APP_BLOG_UI || 'https://ik.am'
+} : {
+    BLOG_API: 'https://api.ik.am',
+    BLOG_UI: 'https://ik.am'
 };
 export default urlMap;
