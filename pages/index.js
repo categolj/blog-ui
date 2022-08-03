57b45f5d7a9c7306b90565038b169233fdd73c20
Toshiaki Maki <tmaki@vmware.com>
2022-08-04 00:26:13 +0900

    Increase fetch size

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const fetchPresentations = () => fetch('https://raw.githubusercontent.com/catego
     .then(data => data.json());
 
 export default function Home({entries}) {
-    const {data: latest, error: errorLatest} = useSWR({size: 10}, entriesFetcherRSocket);
+    const {data: latest, error: errorLatest} = useSWR({size: 15}, entriesFetcherRSocket);
     const {data: github, error: errorGithub} = useSWR('/github', fetchGithub);
     const {data: presos, error: errorPresos} = useSWR('/presos', fetchPresentations);
     return (
