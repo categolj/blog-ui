f74474f4b96c3d802de83a114f11a7780e24ad70
Toshiaki Maki <tmaki@vmware.com>
2022-08-23 18:18:36 +0900

    Change URL

--- utils/urlProvider.js
+++ utils/urlProvider.js
@@ -1,8 +1,8 @@
 const urlMap = (typeof window === 'object') ? {
-    BLOG_API: (process.env.REACT_APP_BLOG_API && process.env.REACT_APP_BLOG_API.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname.replace(".", "-api.")}` : process.env.REACT_APP_BLOG_API || 'https://api.ik.am',
-    BLOG_UI: (process.env.REACT_APP_BLOG_UI && process.env.REACT_APP_BLOG_UI.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname}` : process.env.REACT_APP_BLOG_UI || 'https://ik.am'
+    BLOG_API: (process.env.REACT_APP_BLOG_API && process.env.REACT_APP_BLOG_API.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname.replace(".", "-api.")}` : process.env.REACT_APP_BLOG_API || 'https://blog-api-blog.apps.ik.am',
+    BLOG_UI: (process.env.REACT_APP_BLOG_UI && process.env.REACT_APP_BLOG_UI.toLowerCase() === "auto") ? `${document.location.protocol}//${document.location.hostname}` : process.env.REACT_APP_BLOG_UI || 'https://blog-ui-blog.apps.ik.am'
 } : {
-    BLOG_API: 'https://api.ik.am',
-    BLOG_UI: 'https://ik.am'
+    BLOG_API: 'https://blog-api-blog.apps.ik.am',
+    BLOG_UI: 'https://blog-ui-blog.apps.ik.am'
 };
 export default urlMap;
