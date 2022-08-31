661f3cbfa03b9fd6e338dc8fc09050cdc1f53016
Toshiaki Maki <tmaki@vmware.com>
2022-08-31 15:58:09 +0900

    Add feed

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import {Head, Html, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head/>
+            <Head>
+                <link rel="alternate" type="application/rss+xml" title="IK.AM"
+                      href="/feed"/>
+            </Head>
             <body className="dark:bg-black">
             <Main/>
             <NextScript/>
