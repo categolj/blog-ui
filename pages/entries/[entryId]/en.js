3484091cb5b43a6617fa1f82ef92bc1dc6dae9ad
Toshiaki Maki <tmaki@vmware.com>
2022-09-13 13:39:52 +0900

    Update

--- pages/entries/[entryId]/en.js
+++ pages/entries/[entryId]/en.js
@@ -1,12 +1,17 @@
 import {useRouter} from "next/router";
+import {formatId} from "../index";
 
 export default function English() {
     const router = useRouter();
     let {entryId} = router.query;
-    const url = `https://blog.ik.am/entries/${entryId}/en`;
+    const url = `https://github-com.translate.goog/making/blog.ik.am/blob/master/content/${formatId(entryId)}.md?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=ja&_x_tr_pto=wapp`;
     return (
         <div>
-            <article>Please visit <a href={url}>{url}</a></article>
+            <article>
+                Sorry, the translation page is not currently supported.<br/>
+                Please use Google Translate bellow:<br/>
+                <a href={url}>{url}</a>
+            </article>
         </div>
     )
 }
