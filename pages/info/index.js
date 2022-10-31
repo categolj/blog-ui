52f42cd52c57edfb27176bb81730ede0cc232c34
Toshiaki Maki <tmaki@vmware.com>
2022-10-31 09:31:31 +0900

    Add json-to-html-table

--- pages/info/index.js
+++ pages/info/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import {fetchInfo} from "../../utils/fetcherHttp";
 import useSWR from "swr";
+import {JSONToHTMLTable} from "@kevincobain2000/json-to-html-table";
 
 export default function Index() {
     const {data: info, error: errorInfo} = useSWR('/info', fetchInfo);
-
     return <>
         <Head>
             <title>Info - IK.AM</title>
         </Head>
         <h2>Info</h2>
         <h3>Blog API</h3>
-        <pre><code>{JSON.stringify(info, '\t', 1)}</code></pre>
+        <JSONToHTMLTable data={info || []} tableClassName="info-table"/>
     </>;
 }
