5008e52e85003a9c600a61689696bd947f5a94ce
Toshiaki Maki <tmaki@vmware.com>
2022-12-01 03:19:06 +0900

    Add tag sorting

--- pages/tags/index.js
+++ pages/tags/index.js
@@ -4,9 +4,14 @@ import Tag from "../../components/tag";
 import Loading from "../../components/loading";
 import Head from "next/head";
 import {NextSeo} from "next-seo";
+import React, {useState} from 'react';
+
 
 export default function Tags({}) {
     const {data, error} = useSWR('/tag', fetchTags);
+    const [orderBy, setOrderBy] = useState('name');
+    const buttonStyles = {'name': {}, 'count': {}};
+    buttonStyles[orderBy]['font-weight'] = 'bold';
     return <div>
         <NextSeo title='Tags'
                  canonical='https://ik.am/tags'
@@ -17,8 +22,22 @@ export default function Tags({}) {
             <title>Tags - IK.AM</title>
         </Head>
         <h2>Tags</h2>
+        sort by <button onClick={() => setOrderBy('name')}
+                        style={buttonStyles.name}>name</button>&nbsp;
+        <button onClick={() => setOrderBy('count')} style={buttonStyles.count}>count
+        </button>
         {data ?
-            <ul>{data.map(tag => <li key={tag.name}><Tag name={tag.name}/></li>)}</ul>
+            <ul>{data
+                .sort((a, b) => {
+                    if (orderBy === 'name') {
+                        return a[orderBy].localeCompare(b[orderBy]);
+                    }
+                    if (orderBy === 'count') {
+                        return b[orderBy] - a[orderBy];
+                    }
+                })
+                .map(tag => <li key={tag.name}><Tag name={tag.name}/> ({tag.count})
+                </li>)}</ul>
             : <Loading/>}
     </div>;
 }
