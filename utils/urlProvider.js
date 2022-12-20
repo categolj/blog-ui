b510bdb30195ddf5bf222b8f846982b30760326e
Toshiaki Maki <tmaki@vmware.com>
2022-12-20 10:26:14 +0900

    Add NOTE_API to urlMap

--- utils/urlProvider.js
+++ utils/urlProvider.js
@@ -1,4 +1,5 @@
 const DEFAULT_API_URL = 'https://blog-api-blog.apps.ik.am';
+const DEFAULT_NOTE_URL = 'https://note-api-note.apps.ik.am';
 const DEFAULT_ZIPKIN_URL = 'https://zipkin.ik.am';
 
 const replaceUiToApi = url => {
@@ -21,11 +22,25 @@ const replaceUiToZipkin = url => {
     return DEFAULT_ZIPKIN_URL;
 }
 
+const replaceUiToNote = url => {
+    if (url === 'http://localhost') {
+        return 'http://localhost:9000';
+    }
+    if (url.startsWith('http://blog') || url.startsWith('https://blog')) {
+        const protocol = url.split(':')[0];
+        const parts = url.split('.');
+        return `${protocol}://note-api-note.${parts.slice(1, parts.length).join('.')}`;
+    }
+    return DEFAULT_NOTE_URL;
+}
+
 const urlMap = (typeof window === 'object') ? {
     BLOG_API: replaceUiToApi(`${document.location.protocol}//${document.location.hostname}`),
+    NOTE_API: replaceUiToNote(`${document.location.protocol}//${document.location.hostname}`),
     ZIPKIN_API: replaceUiToZipkin(`${document.location.protocol}//${document.location.hostname}`),
 } : {
     BLOG_API: DEFAULT_API_URL,
+    NOTE_API: DEFAULT_NOTE_URL,
     ZIPKIN_API: DEFAULT_ZIPKIN_URL
 };
 
