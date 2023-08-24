e466e704861aec110bac1a6e47bfccb55339e8e7
Toshiaki Maki <makingx@gmail.com>
2023-08-24 13:31:51 +0900

    Remove zipkin from blog-ui

--- utils/fetcherHttp.js
+++ utils/fetcherHttp.js
@@ -1,36 +1,5 @@
 import urlProvider from "./urlProvider";
 
-process.hrtime = require('browser-process-hrtime');
-
-const {
-    Tracer, BatchRecorder, jsonEncoder: {JSON_V2}, ExplicitContext
-} = require('zipkin');
-const {HttpLogger} = require('zipkin-transport-http');
-
-const determineServiceName = () => {
-    const defaultServiceName = 'blog-ui';
-    if ((typeof window === 'object')) {
-        if (document.location.hostname.endsWith('maki.lol')) {
-            return `${defaultServiceName}-dev`;
-        }
-        if (document.location.hostname.endsWith('ik.am')) {
-            return defaultServiceName;
-        }
-        return `${defaultServiceName}-local`;
-    } else {
-        return defaultServiceName;
-    }
-};
-const tracer = new Tracer({
-    ctxImpl: new ExplicitContext(), recorder: new BatchRecorder({
-        logger: new HttpLogger({
-            endpoint: `${urlProvider.ZIPKIN_API}/api/v2/spans`, jsonEncoder: JSON_V2
-        })
-    }), traceId128Bit: true, localServiceName: determineServiceName()
-});
-const wrapFetch = require('zipkin-instrumentation-fetch');
-export const zipkinFetch = wrapFetch(fetch, {tracer});
-
 const tenantPrefix = (tenantId) => {
     return tenantId ? `/tenants/${tenantId}` : '';
 };
@@ -52,25 +21,25 @@ export async function fetchEntries(data, tenantId) {
     if (params.get('categories') === 'undefined') {
         return [];
     }
-    return zipkinFetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/entries?${params}`, basicAuth).then(res => res.json()).then(j => j.content);
+    return fetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/entries?${params}`, basicAuth).then(res => res.json()).then(j => j.content);
 }
 
 export async function fetchEntry(entryId, tenantId) {
-    return zipkinFetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/entries/${entryId}`, basicAuth).then(res => res.json());
+    return fetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/entries/${entryId}`, basicAuth).then(res => res.json());
 }
 
 export async function fetchTags(tenantId) {
-    return zipkinFetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/tags`, basicAuth).then(res => res.json());
+    return fetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/tags`, basicAuth).then(res => res.json());
 }
 
 export async function fetchCategories(tenantId) {
-    return zipkinFetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/categories`, basicAuth).then(res => res.json());
+    return fetch(`${urlProvider.BLOG_API}${tenantPrefix(tenantId)}/categories`, basicAuth).then(res => res.json());
 }
 
 export async function fetchInfo() {
-    return zipkinFetch(`${urlProvider.BLOG_API}/info`).then(res => res.json());
+    return fetch(`${urlProvider.BLOG_API}/info`).then(res => res.json());
 }
 
 export async function fetchNoteInfo() {
-    return zipkinFetch(`${urlProvider.NOTE_API}/info`).then(res => res.json());
+    return fetch(`${urlProvider.NOTE_API}/info`).then(res => res.json());
 }
